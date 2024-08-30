936dff06095498d54910c76a079650872255b59e
Niloofar-ejtahed <122096961+Niloofar-ejtahed@users.noreply.github.com>
2024-08-30 13:01:41 +0000

    create footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Footer from "./components/footer";
 import Header from "./components/header";
 
 function App() {
   return (
-    <Header />
+    <div className="relative h-fit">
+      <Header />
+    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Nisi blandit erat at dui quisque suspendisse. Massa ullamcorper blandit maximus iaculis suspendisse dignissim in fames. Pellentesque etiam nec dapibus et libero ornare pulvinar. Suspendisse ultricies consectetur lacus sollicitudin gravida cras interdum. Sapien lectus congue nam purus cras ut eros. Vehicula maximus egestas ligula id vulputate nam non congue. Pulvinar bibendum non, phasellus fames a leo eros fringilla senectus. Netus auctor sem ullamcorper dolor malesuada cras.
+
+Leo habitant taciti rutrum suspendisse nulla vel ut gravida. Lectus sociosqu nisl vehicula duis conubia suspendisse feugiat id. Vestibulum auctor dignissim pretium; elit habitasse class. Donec duis augue vitae vel pharetra velit augue. Lacus torquent ultricies quisque sed finibus posuere parturient ultricies? Aliquet lobortis ornare vulputate bibendum eros vitae erat litora. Tempor fames ad iaculis vivamus fames posuere.
+
+Ligula cursus fringilla eu lacinia tristique. Amet pretium aptent morbi interdum libero. Malesuada ipsum aptent dictum aptent penatibus eleifend quis primis. Facilisis orci vestibulum pretium integer facilisis orci quis vehicula. Vel morbi venenatis massa lectus lobortis dui. Quis purus class duis adipiscing vel magna. Per a justo phasellus nunc velit aenean pretium, lorem consectetur.
+
+Augue facilisi proin scelerisque orci curae in est. Aenean gravida augue rutrum viverra inceptos sit diam malesuada. Mus elit magna purus, venenatis purus commodo? Ad blandit porttitor fusce proin purus orci lacus nam. Lorem sociosqu urna vulputate nam sodales dui neque luctus. Gravida elementum metus etiam fermentum lobortis. Ante vestibulum blandit lectus dictum curabitur hendrerit etiam.
+
+Dui potenti integer finibus fringilla arcu erat sodales. Diam vitae molestie nam aptent urna duis eu mi. Convallis aenean nisi natoque lobortis elit ultricies convallis consequat. Erat nisi tempor aliquet platea aliquam. Bibendum senectus dis egestas dignissim iaculis quisque. Gravida vitae vivamus ipsum imperdiet orci consectetur platea. Penatibus viverra semper mauris fringilla; praesent faucibus aliquet.</p>
+      <Footer />
+    </div>
   )
 }
 
