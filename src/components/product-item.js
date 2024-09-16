b180b297ded2b6a7217ffdaf719cb302cd96aafe
Niloofar-ejtahed <122096961+Niloofar-ejtahed@users.noreply.github.com>
2024-09-16 08:55:45 +0000

    refactore

--- src/components/product-item.js
+++ src/components/product-item.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useAsync from "../hooks/useAsync";
-// import Rating from "./rating";
 
 export default function ProductItem() {
   const params = useParams();
@@ -18,7 +17,6 @@ export default function ProductItem() {
         <h2>{data?.title}</h2>
         <p>{data?.description}</p>
         <div className="flex items-center overflow-auto">
-          {/* <Rating rate={data?.rating?.rate} /> */}
           <span className="mx-2">{data?.rating?.count}</span>
         </div>
         <span className="my-5 mx-1 rounded-md	 bg-red-100 text-red-400 p-3 inline-block">
