d414f7be6961a1a3d4633930433d7cb77e74bbc8
Niloofar-ejtahed <122096961+Niloofar-ejtahed@users.noreply.github.com>
2024-09-16 06:33:03 +0000

    fetch products data

--- src/components/shop.js
+++ src/components/shop.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 export default function Shop() {
+
+
+useEffect(()=>{
+  fetch('https://fakestoreapi.com/products',{
+    method : 'Get',
+  }).then(res=>res.json())
+  .then(json=>console.log(json))
+})
+
   return (
     <div>Shop</div>
   )
