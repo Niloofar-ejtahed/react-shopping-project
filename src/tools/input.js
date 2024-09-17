afe706f8d59246e66fa73f59da6cea9057de4ccc
Niloofar-ejtahed <122096961+Niloofar-ejtahed@users.noreply.github.com>
2024-09-17 14:21:28 +0000

    create + use input component

---
+++ src/tools/input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Input(props) {
+
+    const {label , type , id , ...otherProps} = props
+
+  return (
+    <div>
+        <label className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
+        <input type={type || 'text'} id={id} {...otherProps}
+        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+    </div>
+  )
+}
