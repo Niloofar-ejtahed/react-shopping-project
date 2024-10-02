79d05da233a5167be7889bfa8bbd5adfbc96a2ef
Niloofar-ejtahed <122096961+Niloofar-ejtahed@users.noreply.github.com>
2024-10-02 19:02:04 +0000

    add buttons to basket modal

--- src/components/basket-popup.js
+++ src/components/basket-popup.js
@@ -5,32 +5,38 @@ import { useSelector } from 'react-redux';
 export default function BasketPopup(props) {
 
     const data = props?.productData
-    const quantity =props?.quantity
+    const quantity = props?.quantity
 
     const basketData = useSelector((state) => state.basket);
 
     return (
-        <dialog id="my_modal_3" className="modal">
+        <dialog id="my_modal_3" className="modal" >
             <div className="modal-box flex">
 
                 <form method="dialog">
                     <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                 </form>
-                <div className="w-1/4 ">
+
+                <div className="w-1/4 mr-4">
                     <img className="w-60 rounded-lg mx-auto" src={data?.image} />
                 </div>
 
                 <div className="w-3/4 text-left">
-                    <h2 className="text-2xl font-semibold">{data?.title}</h2>
-                    {/* <Rating rate={data?.rating?.rate} />
-                    <p>{data?.rating?.rate}/5</p> */}
-                    <h4 className='text-2xl font-medium py-4'>{data?.price}$</h4>
 
-                    <a className="mt-6 cursor-pointer bg-blue-100 hover:bg-blue-200 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 inline-flex items-center justify-center">
-                        {data?.category}
-                    </a>
+                    <div className='text-success font-medium pb-4'>This product has been added to the cart!</div>
 
-                    <p>{quantity}</p>
+                    <h2 className="font-semibold">{data?.title}</h2>
+
+                    <h4 className='text-xl font-medium py-4'>{data?.price}$</h4>
+
+                    <div className='flex justify-between'>
+                        <div
+                            className="rounded-full bg-orange-500 p-2 text-center" style={{ width: '40px' }}>
+                            {quantity}
+                        </div>
+                        <button className="bg-gray-300 hover:bg-orange-400 text-gray-800  py-2 px-4 rounded ml-4">Go to Basket</button>
+                    </div>
+
 
                 </div>
             </div>
