72c1ce6311d2495a5eab5f91d08deba972f492f9
Niloofar-ejtahed <122096961+Niloofar-ejtahed@users.noreply.github.com>
2024-10-04 19:22:41 +0000

    add totalPrice to store

--- src/redux/reducers/basket.js
+++ src/redux/reducers/basket.js
@@ -1,13 +1,14 @@
 import { createReducer } from "@reduxjs/toolkit";
 import { BasketStateAction } from "./actions";
 
-const initialState = { userId: null, date: undefined, products: []=[] }
+const initialState = { userId: null, date: undefined, products: [] = [] , total :null }
 
 export const BasketReducer = createReducer(initialState, (builder) => {
     builder.addCase(BasketStateAction, (state, action) => {
         state.userId = action.payload.userId;
         state.date = action.payload.date;
         state.products = action.payload.products;
+        state.total = action.payload.total;
     })
 
 })
