f24979c8bc460597504c5f0a9670f539fd17901c
Niloofar-ejtahed <122096961+Niloofar-ejtahed@users.noreply.github.com>
2024-10-05 06:02:18 +0000

    delete console.log

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -19,7 +19,6 @@ export default function UseAsync() {
             .then(res => res.json())
             .then((json) => {
                 setData(json);
-                console.log('data',json)
                 setLoading(false)
             })
             .catch((err) => {
